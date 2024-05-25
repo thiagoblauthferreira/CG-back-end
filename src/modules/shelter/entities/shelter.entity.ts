a131df3e0474c38e82c1716aa0df9fa04048dddd
gabriel <gabrielalencar33564@gmail.com>
2024-05-24 22:55:47 -0300

    feat: create shelter entity

---
+++ src/modules/shelter/entities/shelter.entity.ts
@@ -0,0 +1,39 @@
+import { User } from 'src/modules/auth/entities/auth.enity';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Shelter {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  address: string;
+
+  @Column()
+  phone: string;
+
+  @OneToOne(() => User)
+  @JoinColumn()
+  user: User;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+}
