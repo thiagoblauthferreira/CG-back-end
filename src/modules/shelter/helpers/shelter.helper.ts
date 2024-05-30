3e75f5a3d57d3ab0856af4e45ac62167a5c06b5b
eduardo <educcarnn@gmail.com>
2024-05-30 14:21:58 -0300

    fix: names pt/br

--- src/modules/shelter/helpers/shelter.helper.ts
+++ src/modules/shelter/helpers/shelter.helper.ts
@@ -1,8 +1,8 @@
 export const ShelterMessagesHelper = {
-  SHELTER_NOT_FOUND: 'Shelter not found',
-  SHELTER_DELETED: 'Shelter desativado',
+  SHELTER_NOT_FOUND: 'Abrigo não encontrado',
+  SHELTER_DELETED: 'Abrigo desativado',
   SHELTER_ONLY_COORDINATORS_CAN_CREATE_A_SHELTER:
-    'Only coordinators can create a shelter',
+    'Somente coordenadores podem criar um abrigo',
   SHELTER_COORDINATOR_IS_ALREADY_ASSOCIATED:
-    'This coordinator is already associated with the shelter',
+    'Este coordenador já está associado ao abrigo',
 };
