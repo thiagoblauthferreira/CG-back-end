0f7aaef228885d4622de7d25e4d9bdd398c315b5
Vinicius Gabriel <vinigpo@gmail.com>
2024-06-01 21:58:23 -0300

    Adiciona certificados

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { corsOptions } from './config/cors.options';
+import * as fs from 'fs';
+import * as https from 'https';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: corsOptions });
@@ -15,6 +17,13 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(80);
+  const certPath = './certificados/certificado.pem';
+  const keyPath = './certificados/chave-privada.pem';
+  const cert = fs.readFileSync(certPath);
+  const key = fs.readFileSync(keyPath);
+
+  const httpsOptions = { cert, key };
+  const server = https.createServer(httpsOptions, app.getHttpAdapter().getInstance());
+  server.listen(443);
 }
 bootstrap();
