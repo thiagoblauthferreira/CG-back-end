f83e59a1ddb599acdca14ecdece41ab8fd2dc832
gabriel <gabrielalencar33564@gmail.com>
2024-06-06 01:50:11 -0300

    feat: create news helpers

--- src/modules/shelter/helpers/shelter.helper.ts
+++ src/modules/shelter/helpers/shelter.helper.ts
@@ -3,6 +3,9 @@ export const ShelterMessagesHelper = {
   SHELTER_DELETED: 'Abrigo desativado',
   SHELTER_ONLY_COORDINATORS_CAN_CREATE_A_SHELTER:
     'Somente coordenadores podem criar um abrigo',
-  SHELTER_COORDINATOR_IS_ALREADY_ASSOCIATED:
-    'Este coordenador já está associado ao abrigo',
+  SHELTER_COORDINATOR_ALREADY_ASSOCIATED:
+    'Este coordenador já está associado a este abrigo',
+  COORDINATOR_NOT_FOUND: 'Coordenador não encontrado',
+  USER_NOT_FOUND: 'Usuário não encontrado',
+  THIS_USER_NOT_COORDINATOR: 'Este usuário não é um coordenador',
 };
