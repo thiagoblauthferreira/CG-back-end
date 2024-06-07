8de743e313ad6f9d6c94754581078627de41d1c0
gabriel <gabrielalencar33564@gmail.com>
2024-06-06 22:55:19 -0300

    feat: create news helpers

--- src/modules/products/helpers/product.helper.ts
+++ src/modules/products/helpers/product.helper.ts
@@ -1,4 +1,6 @@
 export const ProductMessagesHelper = {
   PRODUCT_NOT_FOUND: 'Produto não encontrado',
   PRODUCT_DELETED: 'Produto deletado',
+  PRODUCT_ALREADY_ASSOCIATED:
+    'Este produto já está associado a este ponto de distribuição',
 };
