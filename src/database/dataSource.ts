281eb0521adbdc233cdb8320640f559bba32c9aa
eduardo <educcarnn@gmail.com>
2024-07-24 00:47:23 -0300

    feaT: change ssl false

--- src/database/dataSource.ts
+++ src/database/dataSource.ts
@@ -30,7 +30,9 @@ export const dataSourceConfig = (): DataSourceOptions => {
           password: EnvConfig.database.PASSWORD_DB,
           database: EnvConfig.database.NAME_DB,
           synchronize: true,
-          ssl: false
+          ssl: {
+            rejectUnauthorized: false,
+          },
         }),
     entities: [User, Address, Shelter, DistribuitionPoints, Products,  NeedItem, NeedVolunteers, Company,FileEntity],
   };
