853df66c4a24a10ba9130176e46fd0734d8bbf8a
Vinicius Gabriel <vinigpo@gmail.com>
2024-07-24 21:09:47 -0300

    Revert

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ import { EnvConfig } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: corsOptions });
-
+  app.setGlobalPrefix('api');
   const config = new DocumentBuilder()
     .setTitle('Coletivo Gloma - API')
     .setDescription('Coletivo Gloma')
@@ -25,7 +25,11 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
 
   SwaggerModule.setup('api/document', app, document);
-  appConfig(app)
+  appConfig(app);
+  if (EnvConfig.ENV !== "production") {
+
+    await app.listen(8080);
+  } else {
     // Configurações para HTTPS
     const certPath = './certificados/certificado.crt';
     const keyPath = './certificados/chave-privada.pem';
@@ -54,7 +58,7 @@ async function bootstrap() {
       })
       .listen(80);
       app.enableCors(corsOptions);
-  
+  }
 }
 
 bootstrap();
