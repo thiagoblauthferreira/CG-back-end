a760efac8892c2733e9e2fb65c7e8017ecafa27c
eduardo <educcarnn@gmail.com>
2024-08-20 02:05:56 -0300

    feaT: change for return jwt info user

--- src/database/dataSource.ts
+++ src/database/dataSource.ts
@@ -18,9 +18,7 @@ export const dataSourceConfig = (): DataSourceOptions => {
       ? {
           url: EnvConfig.database.URL,
           synchronize: false,
-          ssl: {
-            rejectUnauthorized: false,
-          },
+          ssl: false,
         }
       : {
     type: "postgres",
@@ -30,9 +28,7 @@ export const dataSourceConfig = (): DataSourceOptions => {
           password: EnvConfig.database.PASSWORD_DB,
           database: EnvConfig.database.NAME_DB,
           synchronize: true,
-          ssl: {
-            rejectUnauthorized: false,
-          },
+          ssl: false
         }),
     entities: [User, Address, Shelter, DistribuitionPoints, Products,  NeedItem, NeedVolunteers, Company,FileEntity],
   };
--- src/main.ts
+++ src/main.ts
@@ -1,65 +1,20 @@
 import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { corsOptions } from './config/cors.options';
-import * as fs from 'fs';
-import * as https from 'https';
-import * as http from 'http';
-import { appConfig } from './config/app.config';
-import { EnvConfig } from './config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: corsOptions });
+  const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
 
   const config = new DocumentBuilder()
     .setTitle('Coletivo Gloma - API')
     .setDescription('Coletivo Gloma')
     .setVersion('1.0')
-    .addBearerAuth({ type: 'http', scheme: 'bearer', bearerFormat: 'JWT' })
-    .addTag('Auth')
-    .addTag('Shelter')
-    .addTag('Hello World')
-    .addTag('Distribution points')
-    .addTag('Products')
+    .addTag('tag')
     .build();
   const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
-  SwaggerModule.setup('api/document', app, document);
-  appConfig(app);
-  if (EnvConfig.ENV !== "production") {
-
-    await app.listen(8080);
-  } else {
-    // Configurações para HTTPS
-    const certPath = './certificados/certificado.crt';
-    const keyPath = './certificados/chave-privada.pem';
-    const cert = fs.readFileSync(certPath);
-    const key = fs.readFileSync(keyPath);
-
-    const httpsOptions = {
-      cert: cert,
-      key: key,
-      passphrase: 'gloma',
-    };
-
-    // Criar servidor HTTPS
-    const server = https.createServer(
-      httpsOptions,
-      app.getHttpAdapter().getInstance(),
-    );
-    server.listen(443);
-
-    // Redirecionar HTTP para HTTPS
-    http
-      .createServer((req, res) => {
-        res.writeHead(301, { Location: `https://${req.headers.host}${req.url}` });
-        res.end();
-        
-      })
-      .listen(80);
-      app.enableCors(corsOptions);
-  }
+  await app.listen(80);
 }
-
 bootstrap();
