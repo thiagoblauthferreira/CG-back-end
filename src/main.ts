13cf5e523c04b66f5d99fce883f77110ffed8156
eduardo <educcarnn@gmail.com>
2025-02-03 09:22:01 -0300

    feat: change in main ts

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,14 @@
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { corsOptions } from './config/cors.options';
-import * as fs from 'fs';
-import * as https from 'https';
-import * as http from 'http';
+
 import { appConfig } from './config/app.config';
-import { EnvConfig } from './config';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: corsOptions });
+  app.setGlobalPrefix('api');
 
   const config = new DocumentBuilder()
     .setTitle('Coletivo Gloma - API')
@@ -19,33 +18,16 @@ async function bootstrap() {
     .addTag('Auth')
     .addTag('Shelter')
     .addTag('Hello World')
+    .addTag('Distribution points')
+    .addTag('Products')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+
+  SwaggerModule.setup('api/document', app, document);
   appConfig(app);
 
-  if (EnvConfig.ENV === 'development') return app.listen(8080);
-
-  const certPath = './certificados/certificado.crt';
-  const keyPath = './certificados/chave-privada.pem';
-  const cert = fs.readFileSync(certPath);
-  const key = fs.readFileSync(keyPath);
-
-  const httpsOptions = {
-    cert: cert,
-    key: key,
-    passphrase: 'gloma',
-  };
-  const server = https.createServer(
-    httpsOptions,
-    app.getHttpAdapter().getInstance(),
-  );
-  server.listen(443);
-  http
-    .createServer((req, res) => {
-      res.writeHead(301, { Location: `https://${req.headers.host}${req.url}` });
-      res.end();
-    })
-    .listen(80);
+  await app.listen(3000);
+ 
 }
+
 bootstrap();
